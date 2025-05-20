128036d349db62bb35496b52da48511f801e72e0
easyhooon <mraz3068@gmail.com>
2025-05-20 17:24:43 +0900

    chore: UiState 내 사용하지 않는 프로퍼티 제거

--- feature/list/src/main/kotlin/com/easyhooon/pokedex/feature/list/viewmodel/ListUiState.kt
+++ feature/list/src/main/kotlin/com/easyhooon/pokedex/feature/list/viewmodel/ListUiState.kt
@@ -2,6 +2,4 @@ package com.easyhooon.pokedex.feature.list.viewmodel
 
 data class ListUiState(
     val isLoading: Boolean = false,
-    val isServerErrorDialogVisible: Boolean = false,
-    val isNetworkErrorDialogVisible: Boolean = false,
 )
