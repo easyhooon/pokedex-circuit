bf6b136f60103a9d26dd39bc87fb85b09451578c
easyhooon <mraz3068@gmail.com>
2025-05-23 07:14:49 +0900

    refactor: collectAsState -> collectAsRetainedState

--- feature/favorites/src/main/kotlin/com/easyhooon/pokedex/feature/favorites/FavoritesPresenter.kt
+++ feature/favorites/src/main/kotlin/com/easyhooon/pokedex/feature/favorites/FavoritesPresenter.kt
@@ -1,12 +1,12 @@
 package com.easyhooon.pokedex.feature.favorites
 
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import com.easyhooon.pokedex.core.data.api.repository.PokemonRepository
 import com.easyhooon.pokedex.favorites_detail.FavoritesDetailScreen
 import com.slack.circuit.codegen.annotations.CircuitInject
+import com.slack.circuit.retained.collectAsRetainedState
 import com.slack.circuit.retained.rememberRetained
 import com.slack.circuit.runtime.Navigator
 import com.slack.circuit.runtime.presenter.Presenter
@@ -23,7 +23,7 @@ class FavoritesPresenter @AssistedInject constructor(
     @Composable
     override fun present(): FavoritesScreen.State {
         val isLoading by rememberRetained { mutableStateOf(false) }
-        val favoritesPokemonList by repository.getFavoritesPokemonList().collectAsState(emptyList())
+        val favoritesPokemonList by repository.getFavoritesPokemonList().collectAsRetainedState(emptyList())
 
         return FavoritesScreen.State(
             isLoading = isLoading,
